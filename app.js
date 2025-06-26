793c67a8f92b8be5120bf1a02c117265c8403ba8
Ahmed <ahmad45salim45@gmail.com>
2025-06-26 18:14:14 +0300

    I not working on this project for unknown time

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+dotenv.config();
 
 
 const apiKey = process.env.OPENWEATHER_API_KEY;
@@ -11,12 +12,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3033;
-dotenv.config();
 
 // view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 
 // statics
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,25 +27,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-
 app.post("/weather", async (req, res) => {
-  try {
-    // we get user request
+    try {
+    // we getting user request
     const city = req.body["city"];
-    // we get city lat/lon 
-    const latlonResponse = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`);
-    const lat = latlonResponse.data.lat;
-    const lon = latlonResponse.data.lon;
     
-    // get the weather here and send it back
-    const weatherResponse = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={minutely}&appid=${apiKey}`);
-    res.redirect("index.ejs", {})
+    // we getting city lat/lon 
+    const geoResponse = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
+    const geoData = geoResponse.data[0];
+    const { lat, lon } = geoData;
+
+    // getting the weather here and sending it back to user
+    const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+    res.render("index", { weather: weatherResponse.data, city });
+    console.log(geoData);
   } catch (erorr) {
-    console.log(error.response?.data || error.message);
+    console.log(erorr.response?.data || erorr.message);
     res.status(500).send("Error fetching weather data");
   }
 });
-
 
 app.listen(port, () => {
   console.log(`✅ Server running at http://localhost:${port}`);
